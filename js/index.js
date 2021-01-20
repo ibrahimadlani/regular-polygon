bfaa7452dea370e7614c25359a0b37b63804d992
ibrahimadlani <ibrahim@adlani.com>
2021-01-20 22:56:01 +0100

    chart.js displaying

--- js/index.js
+++ js/index.js
@@ -31,3 +31,38 @@ function pointConstructor(n) {
 
     return data;
 }
+
+var ctx = document.getElementById('chart').getContext('2d');    
+    var chart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: 'i(2π/n)',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132,0.1)',
+                data: pointConstructor(10)
+            }]
+        },
+        options: {
+            hover: {mode: null},
+            tooltips: {enabled: false},
+            scales: {
+                ticks: {display: false},
+                xAxes: [{
+                    gridLines : {
+                        display:false
+                    },
+                    type: 'linear',
+                    position: 'bottom'
+                    
+                }], yAxes: [{
+                    gridLines : {
+                        display:false,
+                    },
+                    type: 'linear',
+                    position: 'bottom'
+                    
+                }]
+            }
+        }
+    });
